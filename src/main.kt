f2a2b378d0eec1b8fad71cc00182989fd7a8ce55
chatSolent <66261650+chatSolent@users.noreply.github.com>
2025-10-30 13:06:30 +0000

    test

--- src/main.kt
+++ src/main.kt
@@ -1,8 +1,11 @@
 fun main() {
     val binnie = Cat("Binnie", 10, 10)
     val clyde = Cat("Clyde", 5, 5)
+    val Rose = Cat("Rose", 2, 4)
     binnie.walk()
     clyde.walk()
+    Rose.walk()
     println(binnie)
     println(clyde)
+    print(Rose)
 }
